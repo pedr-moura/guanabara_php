a4f1fc9c46b78b9fa8993268b40f791a602159cc
Pedro Moura <pedrosousavn@gmail.com>
2024-03-09 13:49:43 -0300

    primeiro ex

---
+++ ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exercicio</title>
+    <style>
+        *{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello, Word! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Printando Hello word no php</p>
+</body>
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados Servidor</title>
+</head>
+<body>
+    <h1>Servidor</h1>
+
+    <?php 
+        phpinfo();
+    ?>
+</body>
+</html>
